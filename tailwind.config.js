0af6fb42604b525f87ce4aa43675283428ba372d
SISADMINISTRATOR <n50284803@gmail.com>
2026-03-26 18:53:15 +0400

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,25 @@
-
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+        serif: ['"PT Serif"', 'serif'],
+      },
+      colors: {
+        blue: {
+          900: '#002B5E',
+          800: '#003A80',
+        },
+        red: {
+          600: '#D52B1E',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
